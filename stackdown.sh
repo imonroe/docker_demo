50c1953f8275037ffd41f95d2110c9258b6bf6d6
Ian Monroe <ian@ianmonroe.com>
2018-12-26 23:25:12 -0600

    tinker

--- stackdown.sh
+++ stackdown.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f ./stack_admin/docker-compose.yml ./stack_application1/docker-compose.yml stop
+docker-compose -f ./stack_admin/docker-compose.yml -f ./stack_application1/docker-compose.yml stop
